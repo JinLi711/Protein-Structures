5277e963d644f10061d5ae1cfa665ef79b9c9668
Jin Li <jinli11@cs.uchicago.edu>
2019-03-13 01:08:31 -0500

    Create get_pdb_ids.py

---
+++ tertiary_structure_prediction/preprocess/get_pdb_ids.py
@@ -0,0 +1,14 @@
+"""
+Given a culled list of PDB files, 
+get only the PDB IDs, ignoring the 
+length, Exptl., resolution, R-factor, and FreeRvalue.
+"""
+
+import sys
+
+with open("../data/cull%i/pdb_ids.dat" % int (sys.argv[1])) as f:
+    pdb_ids = [line.split()[0] for line in f]  
+pdb_ids = ','.join(pdb_ids[1:])
+
+out_file = open("../data/cull%i/pdb_ids.txt" % int (sys.argv[1]), 'w') 
+out_file.write(pdb_ids)
