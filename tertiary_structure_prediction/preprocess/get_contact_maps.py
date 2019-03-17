1678e0c905a1467fc29e08686b7163a41b6bf2b1
Jin Li <jinli11@cs.uchicago.edu>
2019-03-17 17:39:30 -0500

    distance from N to Ns

--- tertiary_structure_prediction/preprocess/get_contact_maps.py
+++ tertiary_structure_prediction/preprocess/get_contact_maps.py
@@ -8,7 +8,8 @@ import numpy as np
 
 def calc_residue_dist(residue_one, residue_two):
     """
-    Returns the C-alpha distance between two residues
+    Returns the C-alpha distance between two residues.
+    Return N distance if C-alpha does not exist.
 
     :param residue_one: single amino acid
     :type  residue_one: Bio.PDB.Residue.Residue
@@ -16,7 +17,25 @@ def calc_residue_dist(residue_one, residue_two):
     :type  residue_two: Bio.PDB.Residue.Residue
     """
 
-    diff_vector = residue_one["CA"].coord - residue_two["CA"].coord
+    try:
+        diff_vector = residue_one["CA"].coord - residue_two["CA"].coord
+    except KeyError:
+        """
+        In some PDB files, it may only contain a few atoms
+        in a residue. This mostly occurs near the beginning or
+        the end of the PDB file because the ends oscillate 
+        a lot, making it difficult for X-ray Crystollagraphy
+        to capture the coordinates.
+        As a result, the there are no CA atoms.
+        Therefore, we will get the first possible atom: N.
+
+        I decided to not ignore this (meaning don't
+        calculate the distance at all) because even though
+        not all positions of the atoms are recorded,
+        it still shows up in the FASTA file of amino acid
+        sequence.
+        """
+        diff_vector = residue_one["N"].coord - residue_two["N"].coord
     return np.sqrt(np.sum(diff_vector * diff_vector))
 
 
@@ -46,6 +65,13 @@ def get_only_residues(chain):
     :type  chain: Bio.PDB.Chain.Chain  
     """
 
+    """
+    HUGE PROBLEM:
+        need to reinsert missing residues, elsewise
+        the contact map is entirely misaligned.
+        Right now, this only calculates contact 
+        for existing residues.
+    """
     new_chain = []
     for residue in chain:
         # http://biopython.org/DIST/docs/tutorial/Tutorial.html#htoc174
