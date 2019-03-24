4598f4defa6c4d4bc2c3a5cd8df0adffa5f5170f
Jin Li <jinli11@cs.uchicago.edu>
2019-03-24 08:20:47 -0400

    Update preprocess.sh

--- tertiary_structure_prediction/scripts/preprocess.sh
+++ tertiary_structure_prediction/scripts/preprocess.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 
+# the integer corresponds to the cull folder
+cullnum=1
+
+
 # convert a culled pdb_ids.dat into just a text file 
 # containing just the PDB IDs
 # example output: pdb_ids.txt  
@@ -8,17 +12,36 @@
 # PDBID1,PDBID2,PDBID3
 # We need this text file for downloading PDB files
 # from the Protein Data Bank Website
-
-# takes an integer as an argument
-# the integer corresponds to the cull folder
-cullnum=1
 python ../preprocess/get_pdb_ids.py $cullnum
+
 
 # Move unwanted pdb files to another folder
 python ../preprocess/remove_unwanted_pdb_files.py $cullnum
+
 
 # Write out a fasta file that contains only the
 # amino acid sequences that we want.
 python ../preprocess/get_wanted_fasta_seq.py $cullnum
 
-python ../preprocess/get_contact_maps.py $cullnum
+
+# write out contact maps
+# python ../preprocess/get_contact_maps.py $cullnum
+
+
+# using the wanted fasta file, write out the amino acid
+# sequence in 1 hot encoded form.
+# python ../preprocess/fasta_to_1_hot_encodings.py $cullnum
+
+
+# Create train, validate, and development test set.
+python ../preprocess/create_model_sets.py $cullnum
+
+
+# remove intermediary files
+# I can actually delete all files in this folder
+# except the final result folder.
+
+# rm ../data/cull$cullnum/amino_acids_1_hot.npy
+# rm ../data/cull$cullnum/contact_map_matrices.npy
+# rm ../data/cull$cullnum/amino_acids.fasta
+# rm -r ../data/cull$cullnum/removed_pdb_files
